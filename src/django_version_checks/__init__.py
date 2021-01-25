dd9babac95066158c04432e462767b34cd6f979a
Adam Johnson <me@adamj.eu>
2021-01-25 10:28:08 +0000

    Support Django 3.2

--- src/django_version_checks/__init__.py
+++ src/django_version_checks/__init__.py
@@ -1,1 +1,4 @@
-default_app_config = "django_version_checks.apps.DjangoVersionChecksAppConfig"
+import django
+
+if django.VERSION < (3, 2):
+    default_app_config = "django_version_checks.apps.DjangoVersionChecksAppConfig"
