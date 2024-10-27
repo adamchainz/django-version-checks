04549d3876b88090d0f367af758edbf424b5915c
Adam Johnson <me@adamj.eu>
2024-10-27 23:30:37 +0000

    Drop Python 3.8 support
    Now EOL: https://discuss.python.org/t/python-3-8-is-now-officially-eol/66983 .

--- src/django_version_checks/typing.py
+++ src/django_version_checks/typing.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 from typing import Callable
-from typing import List
 
 from django.core.checks import CheckMessage
 
-CheckFunc = Callable[..., List[CheckMessage]]
+CheckFunc = Callable[..., list[CheckMessage]]
